77ff60e5ff68e4a2caa9b69261a43ad7854bac91
RyuKeeyeon <bosco315@daum.net>
2024-02-05 12:46:33 +0900

    write anything in here

--- car_control/control/views.py
+++ car_control/control/views.py
@@ -17,13 +17,20 @@ def backward(request):
 
 def left(request):
     mycar.servo_left()
-    time.sleep(1)
+    time.sleep(1)  # 서보 모터를 왼쪽으로 회전시킨 후
+    mycar.motor_forward()  # 자동차를 전진시킵니다.
+    time.sleep(1)  # 실제 환경에서는 이 시간을 조정하여 원하는 회전 각도를 얻을 수 있습니다.
+    mycar.servo_center()  # 회전 후에는 서보 모터를 다시 중앙으로 조정합니다.
     return JsonResponse({'status': 'left turn success'})
 
 def right(request):
     mycar.servo_right()
-    time.sleep(1)
+    time.sleep(1)  # 서보 모터를 오른쪽으로 회전시킨 후
+    mycar.motor_forward()  # 자동차를 전진시킵니다.
+    time.sleep(1)  # 실제 환경에서는 이 시간을 조정하여 원하는 회전 각도를 얻을 수 있습니다.
+    mycar.servo_center()  # 회전 후에는 서보 모터를 다시 중앙으로 조정합니다.
     return JsonResponse({'status': 'right turn success'})
+
 
 def stop(request):
     mycar.motor_stop()
